db1e5806b9eb94d667be82fe01d85a2c2ea1c936
ATGphum <ATGphum@users.noreply.github.com>
2026-09-14 16:21:24 +0000

    deploy: 8572c22c802a001eb1a4d2743c6f9a163b7b1ebe

---
+++ assets/RechargeDesign-Bvctdt3f.js
@@ -0,0 +1,1 @@
+import{a,j as r}from"./index-DqfUiOy7.js";import{A as t}from"./AdoptedRechargePage-CuJoiDUx.js";import"./arrow-left-BKEVrWjD.js";function i(){const{resolvedMode:e}=a();return r.jsx("div",{className:`rc-adopted-frame${e==="dark"?" rc-adopted-night":""}`,children:r.jsx(t,{night:e==="dark"})})}export{i as default};
